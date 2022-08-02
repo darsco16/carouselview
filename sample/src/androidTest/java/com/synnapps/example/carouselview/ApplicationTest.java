df75805a505a3a986aded5dfa8e9a8ff33d5bf25
SAD-12 <darsco16@gmail.com>
2022-08-02 17:41:30 -0500

    try

---
+++ sample/src/androidTest/java/com/synnapps/example/carouselview/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.synnapps.example.carouselview;
+
+import android.app.Application;
+/*import android.test.ApplicationTestCase;
+import androidx.test.runner.*/
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+/*public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}*/
